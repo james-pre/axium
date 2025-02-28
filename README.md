aa7cade97ef68da2b1a338dbfa700a018d81e417
James Prevett <jp@jamespre.dev>
2025-02-27 18:13:02 -0600

    Moved to Svelte (+Kit)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bedrock
+
+Bedrock is a self-hosted open-source alternative to Google Drive
---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+	preprocess: vitePreprocess({ script: true }),
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html',
+			strict: true,
+		}),
+		alias: {
+			$components: 'src/web/components',
+			$stores: 'src/web/stores',
+			$lib: 'src/web/lib',
+		},
+		files: {
+			routes: 'src/web/routes',
+			lib: 'src/web/lib',
+			assets: 'src/web/static',
+		},
+	},
+};
