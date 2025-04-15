29836f02709150443eb6acb4bef9882fd0be0119
James Prevett <jp@jamespre.dev>
2025-04-14 23:18:45 -0500

    Added optional data to `failZod`

--- server/web/utils.ts
+++ server/web/utils.ts
@@ -1,13 +1,14 @@
 import { fail, type ActionFailure } from '@sveltejs/kit';
 import type * as z from 'zod';
 
-export function failZod(error: z.ZodError): ActionFailure<{ error: string }> {
+export function failZod<T extends Record<string, unknown>>(error: z.ZodError, data: T = {} as T): ActionFailure<T & { error: string }> {
 	return fail(400, {
+		...data,
 		error: error.flatten().formErrors[0] || Object.values(error.flatten().fieldErrors).flat()[0],
 	});
 }
 
-export function tryZod<Input, Output>(result: z.SafeParseReturnType<Input, Output>): [Output, null] | [null, ActionFailure<{ error: string }>] {
+export function tryZod<Input, Output>(result: z.SafeParseReturnType<Input, Output>): [Output, null] | [null, ActionFailure<Partial<Output> & { error: string }>] {
 	if (result.success) return [result.data, null];
 	return [null, failZod(result.error)];
 }
