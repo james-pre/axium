849ad5bc518664aa6f872315b230627086c4a043
James Prevett <jp@jamespre.dev>
2025-04-16 14:50:45 -0500

    Updated server exports

--- server/web/index.server.ts
+++ server/web/index.server.ts
@@ -1,3 +1,4 @@
-export * as signup from './routes/signup/+page.server.js';
-export * as name from './routes/name/+page.server.js';
+export * as account from './routes/+page.server.js';
 export * as email from './routes/email/+page.server.js';
+export * as name from './routes/name/+page.server.js';
+export * as signup from './routes/signup/+page.server.js';
