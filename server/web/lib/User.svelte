3cec470a55213ade93f23d4f8ec8919e996a2d27
James Prevett <jp@jamespre.dev>
2025-04-20 13:06:31 -0500

    Add `User` component

---
+++ server/web/lib/User.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import type { User } from '@axium/core/schemas';
+	import { getUserImage } from '@axium/core';
+
+	const {
+		user,
+		compact = false,
+		self = false,
+		href = `/users/${user.id}`,
+		you = false,
+	}: {
+		user: { [K in keyof User]?: User[K] | null };
+		/** If true, don't show the picture */
+		compact?: boolean;
+		/** Whether the user is viewing their own profile */
+		self?: boolean;
+		/** The URL to link to */
+		href?: string;
+		/** Whether to display a "You" label if `self` */
+		you?: boolean;
+	} = $props();
+</script>
+
+<a class={['User', self && 'self']} {href}>
+	{#if !compact}
+		<img src={getUserImage(user as Required<User>)} alt={user.name} />
+	{/if}
+	{user.name}
+	{#if self && you}
+		<span class="subtle">(You)</span>
+	{/if}
+</a>
+
+<style>
+	.User {
+		cursor: pointer;
+		width: max-content;
+		height: max-content;
+	}
+
+	img {
+		width: 2em;
+		height: 2em;
+		border-radius: 50%;
+		vertical-align: middle;
+		margin-right: 0.5em;
+	}
+</style>
