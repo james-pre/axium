be664e7898baa0f0c1b2a16692919a6c6f16a4c0
James Prevett <jp@jamespre.dev>
2025-04-23 17:40:15 -0500

    Fix function

--- cas/src/common.ts
+++ cas/src/common.ts
@@ -1,5 +1,3 @@
-import type { Stats } from '@zenfs/core';
-import { constants as c } from '@zenfs/core';
 import mime from 'mime';
 import fileIcons from './file-icons.json' with { type: 'json' };
 
@@ -15,24 +13,8 @@ export interface FileMetadata {
 	hash: Uint8Array;
 }
 
-export function iconFor(path: string, stats: Stats): string {
-	switch (stats.mode & c.S_IFMT) {
-		case c.S_IFBLK:
-		case c.S_IFCHR:
-			return 'memory';
-		case c.S_IFDIR:
-			return 'folder';
-		case c.S_IFIFO:
-			return 'pipe-section';
-		case c.S_IFLNK:
-			return 'arrow-up-right-from-square';
-		case c.S_IFSOCK:
-			return 'ethernet';
-		case c.S_IFREG:
-		default: {
-			type K = keyof typeof fileIcons;
-			const type = mime.getType(path) || 'application/octet-stream';
-			return fileIcons[type as K] || fileIcons[type.split('/')[0] as K] || 'file';
-		}
-	}
+export function iconFor(path: string): string {
+	type K = keyof typeof fileIcons;
+	const type = mime.getType(path) || 'application/octet-stream';
+	return fileIcons[type as K] || fileIcons[type.split('/')[0] as K] || 'file';
 }
