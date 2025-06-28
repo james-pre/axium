8697acf9dea61e010f2d47a11edcdfa0dfdc1dc4
James Prevett <jp@jamespre.dev>
2025-06-28 16:34:55 -0500

    Fix svelte config

--- server/svelte.config.js
+++ server/svelte.config.js
@@ -1,12 +1,14 @@
-import adapter from '@sveltejs/adapter-node';
+import node from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	runes: true,
+	compilerOptions: {
+		runes: true,
+	},
 	preprocess: vitePreprocess({ script: true }),
 	kit: {
-		adapter: adapter({
+		adapter: node({
 			fallback: 'index.html',
 			strict: true,
 		}),
