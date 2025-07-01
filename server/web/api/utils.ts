b00529015d16f92386c186d3fe72b05fd6a368bb
James Prevett <jp@jamespre.dev>
2025-07-01 10:54:25 -0500

    Add client to CI Small fixes to `getToken` and `checkAuth`

--- server/web/api/utils.ts
+++ server/web/api/utils.ts
@@ -22,15 +22,13 @@ export async function parseBody<const Schema extends z.ZodType, const Result ext
 	}
 }
 
-export function getToken(event: RequestEvent): string | null {
+export function getToken(event: RequestEvent): string | undefined {
 	const header_token = event.request.headers.get('Authorization')?.replace('Bearer ', '');
 	if (header_token) return header_token;
 
 	if (config.debug || config.api.cookie_auth) {
 		return event.cookies.get('session_token');
 	}
-
-	return null;
 }
 
 export async function checkAuth(event: RequestEvent, userId?: string): Promise<void> {
@@ -40,7 +38,7 @@ export async function checkAuth(event: RequestEvent, userId?: string): Promise<v
 
 	const { user } = await getSessionAndUser(token).catch(() => error(401, { message: 'Invalid or expired session' }));
 
-	if (user.id !== userId /* && !user.isAdmin */) error(403, { message: 'User ID mismatch' });
+	if (user?.id !== userId /* && !user.isAdmin */) error(403, { message: 'User ID mismatch' });
 }
 
 export async function createSessionData(event: RequestEvent, userId: string): Promise<NewSessionResponse> {
