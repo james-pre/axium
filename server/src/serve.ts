6210d59a4d97f6bd251372d83527716dfbc271d7
James Prevett <jp@jamespre.dev>
2025-07-02 21:40:09 -0500

    Fix debug SSL stuff

--- server/src/serve.ts
+++ server/src/serve.ts
@@ -1,9 +1,6 @@
 import type { Server } from 'node:http';
 import { createServer } from 'node:http';
 import { createServer as createSecureServer } from 'node:https';
-import { readFileSync } from 'node:fs';
-import { join } from 'node:path/posix';
-import { homedir } from 'node:os';
 
 export interface ServeOptions {
 	secure: boolean;
@@ -15,15 +12,9 @@ export async function serve(opt: Partial<ServeOptions>): Promise<Server> {
 	// @ts-ignore 2307 - The SvelteKit server can be built after `tsc -b` is run
 	const { handler } = await import('../build/handler.js');
 
-	if (!opt.secure) return createServer(handler);
+	if (!opt.secure) return createServer(handler as any);
 
-	return createSecureServer(
-		{
-			key: readFileSync(join(homedir(), '.vite-plugin-mkcert/dev.pem')),
-			cert: readFileSync(join(homedir(), '.vite-plugin-mkcert/cert.pem')),
-		},
-		handler
-	);
+	return createSecureServer({ key: opt.ssl_key, cert: opt.ssl_cert }, handler as any);
 }
 
 export default serve;
