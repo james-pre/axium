4984c4e7fcc7077241c71afeda278a3046b3e5cb
James Prevett <jp@jamespre.dev>
2025-07-05 12:37:37 -0500

    Move icons to assets

--- server/lib/icons/Icon.svelte
+++ server/lib/icons/Icon.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
-	import light from './light.svg';
-	import solid from './solid.svg';
-	import regular from './regular.svg';
-	const urls = { light, solid, regular };
 	const { i } = $props();
-
 	const [style, id] = $derived(i.includes('/') ? i.split('/') : ['solid', i]);
-	const url = $derived(urls[style]);
+	const href = $derived(`/icons/${style}.svg#${id}`);
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em" height="1em">
-	<use href="{url}#{id}" />
+	<use {href} />
 </svg>
 
 <style>
