20d46cbf27fa526de270e89d5d085381059e73c2
James Prevett <jp@jamespre.dev>
2025-07-06 11:33:47 -0500

    Update icon functions Added brands icons

--- server/web/lib/icons/index.ts
+++ server/web/lib/icons/index.ts
@@ -3,8 +3,11 @@ import mimeIcons from './mime.json' with { type: 'json' };
 
 export { default as Icon } from './Icon.svelte';
 
-export function iconFor(path: string): string {
-	type K = keyof typeof mimeIcons;
+export function iconForMime(mimeType: string): string {
+	return mimeIcons[mimeType as keyof typeof mimeIcons] || 'file';
+}
+
+export function iconForPath(path: string): string {
 	const type = mime.getType(path) || 'application/octet-stream';
-	return mimeIcons[type as K] || mimeIcons[type.split('/')[0] as K] || 'file';
+	return iconForMime(type);
 }
