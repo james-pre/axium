e376822871270787fbc590adb44ab6ce58113ec8
James Prevett <jp@jamespre.dev>
2025-07-14 18:39:06 -0500

    Fix bar styles

--- server/web/lib/NumberBar.svelte
+++ server/web/lib/NumberBar.svelte
@@ -4,30 +4,27 @@
 
 <div class="Bar">
 	<div class="fill" style="width: {((value - min) / (max - min)) * 100}%"></div>
-	{#if text}
-		<span class="text">{text}</span>
-	{/if}
+	{#if text}<span class="text">{text}</span>{/if}
 </div>
 
 <style>
 	.Bar {
 		position: relative;
-		border-radius: calc(var(--height, 1em) / 2);
-		height: var(--height, 1em);
+		border-radius: calc(var(--height, 2em) / 2);
+		height: var(--height, 2em);
 		background-color: #444;
+		overflow: hidden;
 	}
 
 	.text {
 		position: absolute;
-		top: 0;
-		left: 1em;
+		inset: calc(calc(var(--height, 2em) - 1em) / 2) 1em;
 		width: fit-content;
-		height: var(--height, 1em);
 		line-height: 1;
 	}
 
 	.fill {
-		border-radius: calc(var(--height, 1em) / 2);
+		border-radius: calc(var(--height, 2em) / 2);
 		height: 100%;
 		background-color: var(--fill, #465);
 	}
