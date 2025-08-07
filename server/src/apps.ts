0e53860b54319f8c9eef6a8a7029a42c3df1f9d1
James Prevett <jp@jamespre.dev>
2025-08-07 11:18:28 -0500

    Tweak `App` creation options

--- server/src/apps.ts
+++ server/src/apps.ts
@@ -1,18 +1,20 @@
 import { pick } from 'utilium';
 import { _unique } from './state.js';
 
-export interface CreateAppOptions {
+export interface AppInit {
 	id: string;
 	name?: string;
+	version?: string;
 }
 
 export const apps = _unique('apps', new Map<string, App>());
 
 export class App {
 	public readonly id!: string;
-	public name!: string;
+	public name?: string;
+	public version?: string;
 
-	public constructor(opt: CreateAppOptions) {
+	public constructor(opt: AppInit) {
 		if (apps.has(opt.id)) throw new ReferenceError(`App with ID "${opt.id}" already exists.`);
 
 		Object.assign(this, pick(opt, 'id', 'name'));
