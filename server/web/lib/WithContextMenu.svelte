9b55b859839836a7f8d840f1326d2763e90d8bdd
James Prevett <jp@jamespre.dev>
2025-08-07 12:56:30 -0500

    Updated `WithContextMenu` to use popovers

--- server/web/lib/WithContextMenu.svelte
+++ server/web/lib/WithContextMenu.svelte
@@ -1,32 +1,73 @@
 <script lang="ts">
-	let { children, menu }: { children(): any; menu(): any } = $props();
+	import type { HTMLAttributes } from 'svelte/elements';
+	import Icon from './Icon.svelte';
 
-	let active = $state(false);
-
-	function oncontextmenu(event: MouseEvent) {
-		event.preventDefault();
-		active = true;
+	interface Props extends HTMLAttributes<HTMLDivElement> {
+		children(): any;
+		menu(
+			/**
+			 * Shortcut to quickly create a generic action in the context menu.
+			 */
+			action: (icon: string, text: string, action: (event: MouseEvent) => void) => any
+		): any;
+		actions: Record<string, () => void>;
 	}
 
-	function onblur() {
-		active = false;
+	let { children, menu, actions, ...rest }: Props = $props();
+
+	let popover = $state<HTMLDivElement>();
+
+	function oncontextmenu(e: MouseEvent) {
+		e.preventDefault();
+		e.stopPropagation();
+		popover!.togglePopover();
+		_forcePopover = true;
+	}
+
+	let _forcePopover = false;
+
+	/**
+	 * Workaround for https://github.com/whatwg/html/issues/10905
+	 * @todo Remove when the problem is fixed.
+	 */
+	function onpointerup(e: PointerEvent) {
+		if (!_forcePopover) return;
+		e.stopPropagation();
+		e.preventDefault();
+		popover!.togglePopover();
+		_forcePopover = false;
 	}
 </script>
 
-<div class="WithContextMenu" {oncontextmenu} tabindex="-1">
-	{@render children()}
+{#snippet action(i: string, text: string, action: (event: MouseEvent) => void)}
+	<div
+		onclick={e => {
+			e.stopPropagation();
+			e.preventDefault();
+			action(e);
+		}}
+		class="action"
+	>
+		{#if i}<Icon {i} --size="14px" />{/if}
+		{text}
+	</div>
+{/snippet}
 
-	{#if active}
-		<div class="menu" {onblur} tabindex="-1">{@render menu()}</div>
-	{/if}
+<div data-axium-context-menu {oncontextmenu} {onpointerup} {...rest}>
+	{@render children()}
+	<div popover bind:this={popover}>
+		{@render menu(action)}
+	</div>
 </div>
 
 <style>
-	.WithContextMenu {
+	[data-axium-context-menu] {
 		display: contents;
 	}
 
-	.menu {
-		position: absolute;
+	div.action:hover {
+		cursor: pointer;
+		background-color: #223;
+		border-radius: 0.25em;
 	}
 </style>
