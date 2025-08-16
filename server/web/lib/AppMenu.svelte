056dd94a27bb340ae38242ddd14163a7f2c97fbc
James Prevett <jp@jamespre.dev>
2025-08-16 12:49:24 -0500

    Use popover menu styling

--- server/web/lib/AppMenu.svelte
+++ server/web/lib/AppMenu.svelte
@@ -11,52 +11,24 @@
 		</button>
 	{/snippet}
 
-	<div class="app-menu">
-		{#await fetchAPI('GET', 'apps')}
-			<i>Loading...</i>
-		{:then apps}
-			{#each apps as app}
-				<a class="app-menu-item icon-text" href="/{app.id}">
-					{#if app.image}
-						<img src={app.image} alt={app.name} width="1em" height="1em" />
-					{:else if app.icon}
-						<Icon i={app.icon} --size="1.5em" />
-					{:else}
-						<Icon i="image-circle-xmark" --size="1.5em" />
-					{/if}
-					<span>{app.name}</span>
-				</a>
-			{:else}
-				<i>No apps available.</i>
-			{/each}
-		{:catch}
-			<i>Couldn't load apps.</i>
-		{/await}
-	</div>
+	{#await fetchAPI('GET', 'apps')}
+		<i>Loading...</i>
+	{:then apps}
+		{#each apps as app}
+			<a class="menu-item" href="/{app.id}">
+				{#if app.image}
+					<img src={app.image} alt={app.name} width="1em" height="1em" />
+				{:else if app.icon}
+					<Icon i={app.icon} --size="1.5em" />
+				{:else}
+					<Icon i="image-circle-xmark" --size="1.5em" />
+				{/if}
+				<span>{app.name}</span>
+			</a>
+		{:else}
+			<i>No apps available.</i>
+		{/each}
+	{:catch}
+		<i>Couldn't load apps.</i>
+	{/await}
 </Popover>
-
-<style>
-	.app-menu {
-		display: flex;
-		flex-direction: column;
-		gap: 0.1em;
-		background-color: #111;
-		border: 1px solid #112;
-		border-radius: 0.5em;
-	}
-
-	.app-menu-item {
-		display: grid;
-		grid-template-columns: 2em 1fr;
-		gap: 1em;
-		padding: 0.5em 1em;
-	}
-
-	.app-menu-item:hover {
-		background-color: #223;
-	}
-
-	.app-menu-item:not(:last-child) {
-		border-bottom: 1px solid #222;
-	}
-</style>
--- storage/lib/Add.svelte
+++ storage/lib/Add.svelte
@@ -16,7 +16,7 @@
 
 {#snippet _item(type: string, text: string, includeContent: boolean = false)}
 	<span
-		class="icon-text add-menu-item"
+		class="menu-item"
 		onclick={() => {
 			createType = type;
 			createIncludesContent = includeContent;
@@ -33,12 +33,10 @@
 		<button class="icon-text"><Icon i="plus" />Add</button>
 	{/snippet}
 
-	<div class="add-menu">
-		<span class="icon-text add-menu-item" onclick={() => uploadDialog.showModal()}><Icon i="upload" />Upload</span>
-		{@render _item('inode/directory', 'New Folder')}
-		{@render _item('text/plain', 'Plain Text')}
-		{@render _item('text/x-uri', 'URL', true)}
-	</div>
+	<span class="menu-item" onclick={() => uploadDialog.showModal()}><Icon i="upload" />Upload</span>
+	{@render _item('inode/directory', 'New Folder')}
+	{@render _item('text/plain', 'Plain Text')}
+	{@render _item('text/x-uri', 'URL', true)}
 </Popover>
 
 <FormDialog
@@ -76,17 +74,7 @@
 </FormDialog>
 
 <style>
-	.add-menu {
-		display: flex;
-		flex-direction: column;
-	}
-
-	.add-menu-item {
-		border-radius: 0.5em;
-		padding: 0.25em 0.25em;
-	}
-
-	.add-menu-item:hover {
+	.menu-item:hover {
 		cursor: pointer;
 		background-color: #4455;
 	}
