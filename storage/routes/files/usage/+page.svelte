0bd54cc130eb05d2e99a82e6a15804ab910cea6e
James Prevett <jp@jamespre.dev>
2025-12-27 11:58:23 -0600

    Remove dead code from /files/usage

--- storage/routes/files/usage/+page.svelte
+++ storage/routes/files/usage/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { Icon, NumberBar } from '@axium/client/components';
+	import { NumberBar } from '@axium/client/components';
+	import '@axium/client/styles/list';
 	import { formatBytes } from '@axium/core/format';
 	import { StorageList } from '@axium/storage/components';
-	import '@axium/client/styles/list';
 
 	const { data } = $props();
 	const { limits } = data.info;
@@ -10,19 +10,12 @@
 	let items = $state(data.info.items.filter(i => i.type != 'inode/directory').sort((a, b) => Math.sign(b.size - a.size)));
 	const usedBytes = $state(data.info.usedBytes);
 
-	let dialogs = $state<Record<string, HTMLDialogElement>>({});
 	let barText = $derived(`Using ${formatBytes(usedBytes)} ${limits.user_size ? 'of ' + formatBytes(limits.user_size * 1_000_000) : ''}`);
 </script>
 
 <svelte:head>
 	<title>Your Storage Usage</title>
 </svelte:head>
-
-{#snippet action(name: string, i: string = 'pen')}
-	<span class="action" onclick={() => dialogs[name].showModal()}>
-		<Icon {i} --size="16px" />
-	</span>
-{/snippet}
 
 <h2>Storage Usage</h2>
 
