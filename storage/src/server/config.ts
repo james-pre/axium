ce2f4863af3f86aa0f57ab302177a7c066511790
James Prevett <jp@jamespre.dev>
2026-01-17 14:28:25 -0600

    Added `extra` validation to audit event types

--- storage/src/server/config.ts
+++ storage/src/server/config.ts
@@ -73,8 +73,20 @@ declare module '@axium/server/audit' {
 	}
 }
 
-addEvent({ source: '@axium/storage', name: 'storage_type_mismatch', severity: Severity.Warning, tags: ['mimetype'] });
-addEvent({ source: '@axium/storage', name: 'storage_size_mismatch', severity: Severity.Warning, tags: [] });
+addEvent({
+	source: '@axium/storage',
+	name: 'storage_type_mismatch',
+	severity: Severity.Warning,
+	tags: ['mimetype'],
+	extra: { item: z.string() },
+});
+addEvent({
+	source: '@axium/storage',
+	name: 'storage_size_mismatch',
+	severity: Severity.Warning,
+	tags: [],
+	extra: { item: z.string().nullable() },
+});
 
 export type ExternalLimitHandler = (userId?: string) => StorageLimits | Promise<StorageLimits>;
 
