55d476c7137c92d7dadfc4c6cc84a39ec4a36381
James Prevett <jp@jamespre.dev>
2026-01-21 21:43:39 -0600

    Removed bad parsing from tasks

--- tasks/routes/tasks/+page.ts
+++ tasks/routes/tasks/+page.ts
@@ -1,5 +1,4 @@
 import { fetchAPI } from '@axium/client/requests';
-import { parseList } from '@axium/tasks/client';
 import { redirect } from '@sveltejs/kit';
 
 export const ssr = false;
@@ -10,8 +9,6 @@ export async function load({ parent }) {
 	if (!session) redirect(307, '/login?after=/tasks');
 
 	const lists = await fetchAPI('GET', 'users/:id/task_lists', {}, session.userId);
-
-	for (const list of lists) parseList(list);
 
 	return { lists };
 }
--- tasks/routes/tasks/[id]/+page.ts
+++ tasks/routes/tasks/[id]/+page.ts
@@ -1,13 +1,10 @@
 import { fetchAPI } from '@axium/client/requests';
 import type { LoadEvent } from '@sveltejs/kit';
-import { parseList } from '@axium/tasks/client';
 
 export const ssr = false;
 
 export async function load({ params }: LoadEvent) {
 	const list = await fetchAPI('GET', 'task_lists/:id', {}, params.id!);
-
-	parseList(list);
 
 	return { list };
 }
