a0987424da2d41415e05caf418d276ff8d976d5b
James Prevett <jp@jamespre.dev>
2026-02-13 22:50:42 -0600

    Switched to using nested media queries

--- client/lib/SidebarLayout.svelte
+++ client/lib/SidebarLayout.svelte
@@ -36,6 +36,10 @@
 		display: grid;
 		grid-template-columns: 15em 1fr;
 		height: 100%;
+
+		@media (width < 700px) {
+			grid-template-columns: 1fr;
+		}
 	}
 
 	.sidebar {
@@ -49,9 +53,33 @@
 		padding-left: 0;
 		border-radius: 0 1em 1em 0;
 
+		@media (width < 700px) {
+			position: fixed;
+			grid-column: unset;
+			inset: auto 0 0;
+			border-radius: 1em;
+			display: flex;
+			flex-direction: row;
+			justify-content: space-around;
+			gap: 1em;
+			padding: 0.5em;
+			z-index: 6;
+
+			.sidebar-text {
+				display: none;
+			}
+		}
+
 		.item {
 			padding: 0.3em 0.5em;
 			border-radius: 0.25em 1em 1em 0.25em;
+
+			@media (width < 700px) {
+				flex: 1 1 0;
+				border-radius: 1em;
+				padding: 1em;
+				justify-content: center;
+			}
 		}
 
 		.item:hover {
@@ -69,44 +97,14 @@
 		padding: 1em;
 		overflow-x: hidden;
 		overflow-y: scroll;
+
+		@media (width < 700px) {
+			padding-bottom: 4em;
+			grid-column: 1;
+		}
 	}
 
 	.sidebar-bottom {
 		margin-top: auto;
-	}
-
-	@media (width < 700px) {
-		.sidebar-container {
-			grid-template-columns: 1fr;
-		}
-
-		.sidebar-content {
-			padding-bottom: 4em;
-			grid-column: 1;
-		}
-
-		.sidebar {
-			position: fixed;
-			grid-column: unset;
-			inset: auto 0 0;
-			border-radius: 1em;
-			display: flex;
-			flex-direction: row;
-			justify-content: space-around;
-			gap: 1em;
-			padding: 0.5em;
-			z-index: 6;
-
-			.item {
-				flex: 1 1 0;
-				border-radius: 1em;
-				padding: 1em;
-				justify-content: center;
-			}
-		}
-
-		.sidebar-text {
-			display: none;
-		}
 	}
 </style>
