67b9cc2734fe7a36ef11f58907909c5a8d8f93dc
James Prevett <jp@jamespre.dev>
2026-02-15 20:55:25 -0600

    Made popover easier to work with

--- client/lib/Popover.svelte
+++ client/lib/Popover.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
 	import Icon from './Icon.svelte';
-	const { children, toggle, showToggle }: { children(): any; toggle?(): any; showToggle?: 'hover' | 'always' } = $props();
+	const {
+		children,
+		toggle,
+		showToggle,
+		popover,
+		...rest
+	}: {
+		children(): any;
+		toggle?(): any;
+		showToggle?: 'hover' | 'always';
+		popover?: 'auto' | 'hint' | 'manual';
+	} & HTMLAttributes<HTMLDivElement> = $props();
 
-	let popover = $state<HTMLDivElement>();
+	let popoverElement = $state<HTMLDivElement>();
 
 	function onclick(e: MouseEvent) {
 		e.stopPropagation();
 		// @ts-expect-error 2345
-		popover?.togglePopover({ source: e.currentTarget });
+		popoverElement?.togglePopover({ source: e.currentTarget });
 	}
 </script>
 
@@ -20,7 +32,7 @@
 		</span>
 	{/if}
 
-	<div popover bind:this={popover}>
+	<div popover={popover || 'auto'} bind:this={popoverElement} {...rest}>
 		{@render children()}
 	</div>
 </div>
