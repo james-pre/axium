942eceb698d4dc463aa5b822f1ade72c0325c39e
James Prevett <jp@jamespre.dev>
2026-03-01 13:53:31 -0600

    Added `getPackage` Moved some package util code out of node directory

--- core/src/node/packages.ts
+++ core/src/node/packages.ts
@@ -1,36 +1,6 @@
 import * as fs from 'node:fs';
 import { findPackageJSON } from 'node:module';
-import { lt as ltVersion } from 'semver';
-import { warn } from '../io.js';
-import type { PackageVersionInfo } from '../packages.js';
-
-function isRelative(specifier: string): boolean {
-	return specifier[0] == '/' || ['.', '..'].includes(specifier.split('/')[0]);
-}
-
-interface NpmPackageVersion {
-	name: string;
-	version: string;
-}
-
-interface NpmPackage {
-	name: string;
-	'dist-tags': Record<string, string>;
-	versions: Record<string, NpmPackageVersion>;
-}
-
-let cacheTTL = 1000 * 60 * 60;
-
-export function setPackageCacheTTL(seconds: number) {
-	cacheTTL = seconds * 1000;
-}
-
-interface CacheEntry {
-	timestamp: number;
-	data: NpmPackage;
-}
-
-const cache = new Map<string, CacheEntry>();
+import { getPackage, type PackageVersionInfo } from '../packages.js';
 
 /**
  * @param name Package name
@@ -41,22 +11,6 @@ export async function getVersionInfo(specifier: string, from: string = import.me
 	if (!path) throw new Error(`Cannot find package.json for package ${specifier} (from ${from})`);
 	const { version, name } = JSON.parse(fs.readFileSync(path, 'utf8'));
 
-	const info: PackageVersionInfo = { name, version, latest: null };
-
-	if (isRelative(specifier)) return info;
-
-	const cached = cache.get(specifier);
-
-	const useCache = cached && Date.now() - cached.timestamp < cacheTTL;
-
-	try {
-		const pkg = useCache ? cached.data : await fetch('https://registry.npmjs.org/' + specifier).then(res => res.json());
-		if (!useCache) cache.set(specifier, { timestamp: Date.now(), data: pkg });
-
-		info.latest = pkg['dist-tags']?.latest || Object.keys(pkg.versions).sort((a, b) => (ltVersion(a, b) ? 1 : -1))[0];
-	} catch (e) {
-		warn(`Failed to fetch version info for package ${name}: ${e instanceof Error ? e.message : String(e)}`);
-	}
-
-	return info;
+	const pkg = await getPackage(name);
+	return { name, version, latest: pkg?._latest };
 }
--- core/src/packages.ts
+++ core/src/packages.ts
@@ -1,9 +1,61 @@
 import * as z from 'zod';
+import { lt as ltVersion } from 'semver';
+import { warn } from './io.js';
 
 export const PackageVersionInfo = z.object({
 	name: z.string(),
 	version: z.string(),
-	latest: z.string().nullable(),
+	latest: z.string().nullish(),
 });
 
 export interface PackageVersionInfo extends z.infer<typeof PackageVersionInfo> {}
+
+export function isRelative(specifier: string): boolean {
+	return specifier[0] == '/' || ['.', '..'].includes(specifier.split('/')[0]);
+}
+
+interface NpmPackageVersion {
+	name: string;
+	version: string;
+}
+
+export interface NpmPackage {
+	name: string;
+	'dist-tags': Record<string, string>;
+	versions: Record<string, NpmPackageVersion>;
+	_latest: string;
+}
+
+let cacheTTL = 1000 * 60 * 60;
+
+export function setPackageCacheTTL(seconds: number) {
+	cacheTTL = seconds * 1000;
+}
+
+interface CacheEntry {
+	timestamp: number;
+	data: NpmPackage;
+}
+
+const cache = new Map<string, CacheEntry>();
+
+/**
+ * Get information for an npm package
+ */
+export async function getPackage(specifier: string): Promise<NpmPackage | null> {
+	if (isRelative(specifier)) return null;
+
+	const cached = cache.get(specifier);
+
+	if (cached && Date.now() - cached.timestamp < cacheTTL) return cached.data;
+
+	try {
+		const pkg = await fetch('https://registry.npmjs.org/' + specifier).then(res => res.json());
+		pkg._latest = pkg['dist-tags']?.latest || Object.keys(pkg.versions).sort((a, b) => (ltVersion(a, b) ? 1 : -1))[0];
+		cache.set(specifier, { timestamp: Date.now(), data: pkg });
+		return pkg;
+	} catch (e) {
+		warn(`Failed to fetch metadata for package ${specifier}: ${e instanceof Error ? e.message : String(e)}`);
+		return cached?.data || null;
+	}
+}
