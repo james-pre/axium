4066cf9bb91225de3746aa4a8a7c5ba437cfe4b6
James Prevett <jp@jamespre.dev>
2026-03-03 14:40:42 -0600

    Added translations for admin dashboard tabs

--- server/routes/admin/+layout.ts
+++ server/routes/admin/+layout.ts
@@ -1,6 +1,7 @@
 import { getCurrentSession } from '@axium/client/user';
 import type { Session, User } from '@axium/core';
 import type { LayoutLoadEvent, LayoutRouteId } from './$types';
+import { text } from '@axium/client';
 
 export const ssr = false;
 
@@ -13,14 +14,19 @@ export async function load({
 	const tabs = [
 		{ name: 'dashboard', href: '/admin', icon: 'gauge', active: route.id.endsWith('/admin') },
 		{
-			name: 'users',
+			name: text('page.admin.tab.users'),
 			href: '/admin/users',
 			icon: 'user-group',
 			active: route.id.endsWith('/admin/users') || route.id.endsWith('/admin/users/[id]'),
 		},
-		{ name: 'configuration', href: '/admin/config', icon: 'sliders', active: route.id.endsWith('/admin/config') },
-		{ name: 'plugins', href: '/admin/plugins', icon: 'puzzle-piece-simple', active: route.id.endsWith('/admin/plugins') },
-		{ name: 'audit log', href: '/admin/audit', icon: 'file-shield', active: route.id.endsWith('/admin/audit') },
+		{ name: text('page.admin.tab.config'), href: '/admin/config', icon: 'sliders', active: route.id.endsWith('/admin/config') },
+		{
+			name: text('page.admin.tab.plugins'),
+			href: '/admin/plugins',
+			icon: 'puzzle-piece-simple',
+			active: route.id.endsWith('/admin/plugins'),
+		},
+		{ name: text('page.admin.tab.audit'), href: '/admin/audit', icon: 'file-shield', active: route.id.endsWith('/admin/audit') },
 	] satisfies { name: string; href: LayoutRouteId; icon: string; active: boolean }[];
 
 	session ||= await getCurrentSession().catch(() => null);
