f2429224287312386c36c145b4209e99ab554253
James Prevett <jp@jamespre.dev>
2026-03-31 19:39:54 -0500

    Updated `Location` to enforce country codes Added `Location` translation keys

--- client/lib/LocationSelect.svelte
+++ client/lib/LocationSelect.svelte
@@ -2,25 +2,19 @@
 	import { text, countryNames } from '@axium/client';
 	import type { LocationInit } from '@axium/core';
 
-	let {
-		value = $bindable<LocationInit>(),
-		locale = 'en',
-	}: {
-		value: LocationInit;
-		locale: string;
-	} = $props();
+	let { value = $bindable<LocationInit>() }: { value: LocationInit } = $props();
 </script>
 
-<select bind:value={value.country} placeholder={text('LocationSelect.country')}>
+<select bind:value={value.country} placeholder={text('location.country')}>
 	{#each Object.entries(countryNames) as [code, name]}
 		<option value={code}>{name}</option>
 	{/each}
 </select>
 
-<input type="text" placeholder={text('LocationSelect.street1')} bind:value={value.street1} />
-<input type="text" placeholder={text('LocationSelect.street2')} bind:value={value.street2} />
-<input type="text" placeholder={text('LocationSelect.locality')} bind:value={value.locality} />
+<input type="text" placeholder={text('location.street1')} bind:value={value.street1} />
+<input type="text" placeholder={text('location.street2')} bind:value={value.street2} />
+<input type="text" placeholder={text('location.locality')} bind:value={value.locality} />
 <div>
-	<input type="text" placeholder={text('LocationSelect.subdivision')} bind:value={value.subdivision} />
-	<input type="text" placeholder={text('LocationSelect.postalCode')} bind:value={value.postalCode} />
+	<input type="text" placeholder={text('location.subdivision')} bind:value={value.subdivision} />
+	<input type="text" placeholder={text('location.postal_code')} bind:value={value.postalCode} />
 </div>
--- core/src/location.ts
+++ core/src/location.ts
@@ -1,17 +1,19 @@
 import * as z from 'zod';
+import countries from '../data/countries.json' with { type: 'json' };
+import { zKeys } from './locales.js';
 
 export const Location = z.object({
 	/** ISO 3166-1 alpha-2 */
-	country: z.string().length(2),
+	country: z.literal(countries).register(zKeys, { key: 'location.country' }),
 	/** ISO 3166-2. state/province/region/etc. */
-	subdivision: z.string().max(255).nullish(),
+	subdivision: z.string().max(255).nullish().register(zKeys, { key: 'location.subdivision' }),
 	/** city/town/municipality */
-	locality: z.string().max(255).nullish(),
+	locality: z.string().max(255).nullish().register(zKeys, { key: 'location.locality' }),
 	/** postal code */
-	postalCode: z.string().max(255).nullish(),
+	postalCode: z.string().max(255).nullish().register(zKeys, { key: 'location.postal_code' }),
 	/** street address */
-	street1: z.string().max(255).nullish(),
-	street2: z.string().max(255).nullish(),
+	street1: z.string().max(255).nullish().register(zKeys, { key: 'location.street1' }),
+	street2: z.string().max(255).nullish().register(zKeys, { key: 'location.street2' }),
 });
 
 export interface Location extends z.infer<typeof Location> {}
