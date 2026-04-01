7366e1f2873a685d66e829b7c09ea8c6adb6ae1d
James Prevett <jp@jamespre.dev>
2026-04-01 18:15:51 -0500

    Rename api/{pfp -> images}.ts

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -2,7 +2,7 @@ import './acl.js';
 import './admin.js';
 import './metadata.js';
 import './passkeys.js';
-import './pfp.js';
+import './images.js';
 import './register.js';
 import './session.js';
 import './users.js';
