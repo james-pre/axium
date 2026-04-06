7c78d9021918f089c2fc717598f93792dfd3764d
James Prevett <jp@jamespre.dev>
2026-04-06 16:45:12 -0500

    Added `ZodLocaleInfo` Added prefix to `zKeys` registry

--- core/src/locales.ts
+++ core/src/locales.ts
@@ -1,6 +1,27 @@
 import { registry as zodRegistry } from 'zod/v4/core';
 
+export interface ZodLocaleInfo {
+	/**
+	 * Translation key for this schema
+	 */
+	key?: string;
+	/**
+	 * Key prefix for objects, enums, and literal collections. Intended for use with `ZodInput` for easier usage.
+	 * @example
+	 * ```ts
+	 * const Duck = z.object({
+	 * 	name: z.string(),
+	 * 	type: z.literal(['mallard', 'gadwall', 'pintail']).register(zKeys, { key: 'duck.type', prefix: 'duck_type' }),
+	 * }).register(zKeys, { key: 'animals.duck', prefix: 'duck' });
+	 *
+	 * text('duck.name'); // 'Duck Name'
+	 * text('duck_type.mallard') // 'Mallard'
+	 * ```
+	 */
+	prefix?: string;
+}
+
 /**
  * Zod registry for attaching translation keys to schemas
  */
-export const zKeys = zodRegistry<{ key: string }>();
+export const zKeys = zodRegistry<ZodLocaleInfo>();
