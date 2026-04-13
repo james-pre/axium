ec44595be1eb1399d5168c95afa5056b877d153d
James Prevett <jp@jamespre.dev>
2026-04-13 17:50:51 -0500

    Added audio info box

--- client/lib/Audio.svelte
+++ client/lib/Audio.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { text } from '@axium/client/locales';
 	import type { Snippet } from 'svelte';
 	import Icon from './Icon.svelte';
 	import MediaControls from './MediaControls.svelte';
@@ -12,7 +13,17 @@
 	const { cover: showCover, extraControls, ...rest }: Props = $props();
 	const { src } = rest;
 
-	const { picture, pictureURL } = await getMetadata(rest);
+	const id = $props.id();
+
+	const { metadata, picture, pictureURL } = await getMetadata(rest);
+
+	const audioInfo = [
+		['music', metadata.common.title],
+		['album', metadata.common.album],
+		['user-music', metadata.common.artist],
+		['hashtag', metadata.common.track.no],
+		['compact-disc', metadata.common.disk.no],
+	] as const;
 
 	const media = new MediaState();
 </script>
@@ -38,8 +49,27 @@
 		bind:buffered={media.buffered}
 		bind:playbackRate={media.playbackRate}
 	></audio>
-	<MediaControls {media} />
+	<MediaControls {media}>
+		<button class="reset icon-text" command="show-modal" commandfor="{id}:audio-info">
+			<Icon i="regular/circle-info" />
+		</button>
+		{#if extraControls}{@render extraControls()}{/if}
+	</MediaControls>
 </div>
+
+<dialog id="{id}:audio-info">
+	<div class="audio-info">
+		{#each audioInfo as [icon, value]}
+			{#if value}
+				<div class="icon-text">
+					<Icon i={icon} />
+					<span>{value}</span>
+				</div>
+			{/if}
+		{/each}
+	</div>
+	<button command="close" commandfor="{id}:audio-info">{text('generic.done')}</button>
+</dialog>
 
 <style>
 	.audio-cover {
@@ -60,5 +90,12 @@
 		:global(.MediaControls) {
 			width: 512px;
 		}
+	}
+
+	.audio-info {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5em;
+		margin-bottom: 1em;
 	}
 </style>
