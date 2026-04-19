efd49dd6b6e9bf3019a375c40dbb154b96c53d22
James Prevett <jp@jamespre.dev>
2026-04-18 21:01:23 -0500

    Added registration support to setup

--- client/lib/native/Setup.svelte
+++ client/lib/native/Setup.svelte
@@ -5,6 +5,9 @@
 	import { errorText } from 'ioium';
 	import { httpUrl as z_httpUrl } from 'zod';
 	import Login from '../Login.svelte';
+	import Register from '../Register.svelte';
+
+	const { isRegister }: { isRegister?: boolean } = $props();
 
 	const { value: existingPrefix } = await Preferences.get({ key: 'api_prefix' });
 
@@ -19,9 +22,10 @@
 			if (!prefixValue) throw 'You must enter an Axium server address';
 			const withProtocol = startsWithProtocol.test(prefixValue) ? prefixValue : 'https://' + prefixValue;
 			const url = new URL(z_httpUrl().parse(withProtocol));
-			if (url.pathname == '/') url.pathname = '/api';
+			if (url.pathname == '/') url.pathname = '/api/';
 			setPrefix(url.href);
 			const apps = await fetchAPI('GET', 'apps');
+			await Preferences.set({ key: 'api_prefix', value: url.href });
 			noValidPrefix = false;
 		} catch (e) {
 			error = errorText(e);
@@ -41,6 +45,8 @@
 
 		<button {onclick}>{text('native.setup.next')}</button>
 	</div>
+{:else if isRegister}
+	<Register fullPage />
 {:else}
 	<Login fullPage />
 {/if}
--- client/src/native/setup.ts
+++ client/src/native/setup.ts
@@ -1,14 +1,17 @@
+import type { Session, User } from '@axium/core';
 import { Preferences } from '@capacitor/preferences';
+import { CapacitorPasskey } from '@capgo/capacitor-passkey';
 import { setPrefix } from '../requests.js';
 import { getCurrentSession } from '../user.js';
-import type { Session, User } from '@axium/core';
 
 export interface LoadResult {
 	session?: (Session & { user: User }) | null;
 	needsSetup?: boolean;
+	/** Whether we are in "register instead" */
+	isRegister: boolean;
 }
 
-export async function loadLayout(): Promise<LoadResult> {
+export async function loadLayout({ url }: { url: URL }): Promise<LoadResult> {
 	const { value: api_prefix } = await Preferences.get({ key: 'api_prefix' });
 
 	let session;
@@ -16,7 +19,9 @@ export async function loadLayout(): Promise<LoadResult> {
 	if (api_prefix) {
 		setPrefix(api_prefix);
 		session = await getCurrentSession().catch(() => null);
+		const { origin } = new URL(api_prefix);
+		CapacitorPasskey.shimWebAuthn({ origin });
 	}
 
-	return { session, needsSetup: !api_prefix || !session };
+	return { session, isRegister: url.pathname == '/register', needsSetup: !api_prefix || !session };
 }
