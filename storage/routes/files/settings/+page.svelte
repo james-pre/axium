26d94d69d4e8dbf06c4668e78aafe7f4642d0f0f
James Prevett <jp@jamespre.dev>
2026-05-15 10:58:10 -0500

    Fixed /files/settings being accessible without login

--- storage/routes/files/settings/+page.svelte
+++ storage/routes/files/settings/+page.svelte
@@ -4,10 +4,12 @@
 	import { UserStoragePreferences } from '@axium/storage/common';
 
 	const { data } = $props();
+
+	if (!data.session) location.href = '/login?after=/files/settings';
 </script>
 
 <svelte:head>
 	<title>{text('page.files.settings.title')}</title>
 </svelte:head>
 
-<AppPreferences userId={data.session.userId} appId="files" schema={UserStoragePreferences} />
+<AppPreferences userId={data.session!.userId} appId="files" schema={UserStoragePreferences} />
