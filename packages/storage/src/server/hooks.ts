3deddc97164852373ea0c5f7ad1299f9900b1755
James Prevett <jp@jamespre.dev>
2026-07-17 10:33:31 -0500

    Switched to `addShortcut`

--- packages/storage/src/server/hooks.ts
+++ packages/storage/src/server/hooks.ts
@@ -2,11 +2,19 @@ import { getConfig } from '@axium/core';
 import { formatBytes } from '@axium/core/format';
 import type { OpOptions } from '@axium/server/database';
 import { count, database } from '@axium/server/database';
+import { addShortcut } from '@axium/server/routes';
 import { track } from 'ioium/node';
 import { mkdirSync } from 'node:fs';
+import { decodeUUID } from 'utilium/string';
+import * as z from 'zod';
 import '../common.js';
+import './api.js';
+import './batch.js';
 import { getTotalUse } from './db.js';
-import './index.js';
+import './raw.js';
+import './zip.js';
+
+addShortcut('/f/:id', { id: z.base64url() }, ({ id }) => `/files/${decodeUUID(Uint8Array.fromBase64(id, { alphabet: 'base64url' }))}`);
 
 export function load() {
 	mkdirSync(getConfig('@axium/storage').data, { recursive: true });
