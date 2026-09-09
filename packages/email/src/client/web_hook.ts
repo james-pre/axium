4795d5f19945a5c12a48e1177f006741a5e6a829
James Prevett <jp@jamespre.dev>
2026-09-09 10:53:05 -0500

    Don't block while connecting to the socket

--- packages/email/src/client/web_hook.ts
+++ packages/email/src/client/web_hook.ts
@@ -3,7 +3,7 @@ import { addListener, connect } from '@axium/client/socket';
 import { toast } from '@axium/client/toast';
 import '../common.js';
 
-await connect().catch(() => null);
+void connect().catch(() => null);
 
 addListener('email.received', email => {
 	if (email.folder != 'spam') void toast('info', text('email.toast_received', { name: email.from.name || email.from.address }));
