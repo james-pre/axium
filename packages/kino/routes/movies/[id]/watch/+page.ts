6e06f0b285981d48a7906203e151a6dd5b3b94b2
James Prevett <jp@jamespre.dev>
2026-09-26 17:24:36 -0500

    When watch a movie, a previous view is now fetched in parallel

--- packages/kino/routes/movies/[id]/watch/+page.ts
+++ packages/kino/routes/movies/[id]/watch/+page.ts
@@ -4,12 +4,13 @@ import { error } from '@sveltejs/kit';
 export async function load({ params }) {
 	const id = Number(params.id);
 
-	const movie = await getMovie(id);
+	const [movie, [view]] = await Promise.all([
+		getMovie(id),
+		// Used to resume where the last session left off; not worth failing the page over
+		getViews({ type: 'movie', id, limit: 1 }).catch(() => []),
+	]);
 
 	if (!movie.upload) error(404, 'This movie has not been uploaded');
-
-	// Used to resume where the last session left off; not worth failing the page over
-	const [view] = await getViews({ type: 'movie', id, limit: 1 }).catch(() => []);
 
 	return { movie, upload: movie.upload, view };
 }
